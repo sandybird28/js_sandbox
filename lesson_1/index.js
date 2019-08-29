fef4f6f7599491235e6c3e3c42cdd874a49dbdc6
sandybird28 <aler2893@gmail.com>
2019-08-29 23:02:36 +0300

    доделал 4 и 5

--- lesson_1/index.js
+++ lesson_1/index.js
@@ -10,7 +10,7 @@ function makeGreetings(age){
                 default: return 'лет';
             }
         } else{
-            return 'лет'
+            return 'лет';
         };        
     }
     return 'Мой возраст '+age+' '+end(age);
@@ -28,6 +28,27 @@ function splitArray(arr, b){
 //3
 function add(x){
     return function(y){
-        return x+y
+        return x+y;
     }
+}
+
+//4
+function transformData(arr){
+    let obj={};
+    for(i=0;i<arr.length;i++){
+        if(arr[i].mark>5){
+            ((arr[i].lastName)&&(arr[i].firstName))? obj[arr[i].login]= arr[i].firstName+' '+arr[i].lastName: obj[arr[i].login]= arr[i].firstName+arr[i].lastName;
+        }
+    }
+   return obj;
+}
+
+//5
+function addInfinite(x){
+    f.result = x;
+    function f(y){
+        f.result += y;
+        return f
+    };
+    return f
 }
