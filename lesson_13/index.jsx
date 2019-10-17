a550a2de13ad375454c193ae404dd46aa4d23b05
sandybird28 <aler2893@gmail.com>
2019-10-17 11:55:24 +0300

    did 3-d part

--- lesson_13/index.jsx
+++ lesson_13/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const location = { lat: 49.5979715, lng: 34.5346919 };
+const color = 'forestgreen'
+
 const Rect = (props) => (
     <div  
         class='main' 
@@ -20,7 +23,8 @@ const Circle = (props) => (
 
 const Address = (props) => (
     <address>
-        <a 
+        <a
+          class='footer-link'
           href={`https://www.google.com/maps/@${props.location.lat},${props.location.lng},18.01z`}>
             {props.street}, {props.city}, {props.country}
         </a>
@@ -28,13 +32,65 @@ const Address = (props) => (
 )
 
 
+const Header = (props) => (
+    <header class='header'>
+        Header
+    </header>
+)
 
-// const User = (props) => (
-//     <div>
-//       Hello, my name is {props.name}, I am {props.age}
-//       I wanna say: {props.children}
-//     </div>
-//   );
+const Content = (props) => (
+    <div class='content'>
+        <Circle
+            color='lightgreen'
+        >
+            Circle
+        </Circle>
+        <User
+            name="John"
+            age={32}
+        >
+            Hello World!
+        </User>
+    </div>
+)
+
+const Sidebar = (props) => (
+    <aside class='sidebar'>
+        <Rect
+        color={color}
+        >
+            Square
+        </Rect>
+    </aside>
+)
+
+
+const Main = (props) => (
+        <main>
+            <Content />
+            <Sidebar />
+        </main>
+)
+
+const Footer = (props) => (
+    <footer class='footer'>
+        <Address
+            country = 'Ukraine'
+            city = 'Poltava'
+            street = 'Sobornosti Street'
+            location = {location}
+        />
+    </footer>
+)
+
+
+
+
+const User = (props) => (
+    <div>
+      Hello, my name is {props.name}, I am {props.age} I wanna say: {props.children}
+    </div>
+  );
 
 
 // const color = 'green'
@@ -58,17 +114,25 @@ const Address = (props) => (
     
 //     document.getElementById('root'),
 // );
-const location = { lat: 49.5979715, lng: 34.5346919 }
 
+
+// ReactDOM.render(
+//     <Address
+//         country = 'Ukraine'
+//         city = 'Poltava'
+//         street = 'Sobornosti Street'
+//         location = {location}
+    
+//     />,
+     
+//     document.getElementById('root'),
+//   );
 
 ReactDOM.render(
-    <Address
-        country = 'Ukraine'
-        city = 'Poltava'
-        street = 'Sobornosti Street'
-        location = {location}
-    
-    />,
-     
+    <div>
+        <Header />
+        <Main />
+        <Footer />    
+    </div>,
     document.getElementById('root'),
   );
